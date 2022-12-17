495ad7c15314ec7f64121bfc591b086d2b61a947
Regera3y <98745495+Regera3y@users.noreply.github.com>
2022-12-17 16:54:58 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+
+Webcam.set({
+    width: 350,
+    heoght: 300,
+    image_format : 'png',
+    png_quality: 90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id = "captured_image" src = "' + data_uri + '"/>'
+    });
+}
